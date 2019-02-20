fec2b7c7e32fbc31b26ea34d9478e10af1013104
grygg <grygg513@163.com>
2019-02-20 23:49:51 +0800

    concat original texts

--- SeqMatchSeq-master/main/main.py
+++ SeqMatchSeq-master/main/main.py
@@ -5,17 +5,19 @@ import torch
 import random
 
 import util.loadFiles as tr
-from wikiqa.compAggWikiqa import compAggWikiqa, train
+from wikiqa.compAggWikiqa import compAggWikiqa, train,pretrain,rl_pretrain
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int, default=10, help='number of sequences to train on in parallel')
-parser.add_argument('--max_epochs', type=int, default=25, help='number of full passes through the training data')
+parser.add_argument('--max_epochs', type=int, default=120, help='number of full passes through the training data')
+parser.add_argument('--pretrain_epochs', type=int, default=50, help='number of full passes through the training data')
 parser.add_argument('--seed', type=int, default=321, help='torch manual random number generator seed')
 parser.add_argument('--reg', type=float, default=0, help='regularize value')
 parser.add_argument('--learning_rate', type=float, default=0.004, help='learning rate')
 parser.add_argument('--emb_lr', type=float, default=0., help='embedding learning rate')
 parser.add_argument('--emb_partial', type=bool, default=True, help='only update the non-pretrained embeddings')
-parser.add_argument('--lr_decay', type=float, default=0.95, help='learning rate decay ratio')
+parser.add_argument('--lr', type=float, default=4e-4, help='learning rate decay ratio')
+parser.add_argument('--lr_decay', type=float, default=0.99, help='learning rate decay ratio')
 parser.add_argument('--dropoutP', type=float, default=0.3, help='dropout ratio')
 parser.add_argument('--expIdx', type=int, default=0, help='experiment index')
 parser.add_argument('--num_classes', type=int, default=2, help='number of classes')
@@ -38,11 +40,15 @@ parser.add_argument('--grad', type=str, default="adamax", help='gradient descent
 
 parser.add_argument('--log', type=str, default="nothing", help='log message')
 parser.add_argument('--gpu', type=bool, default=True, help='use gpu or not')
+parser.add_argument('--pretrained_embed', type=bool, default=True, help='use pretrained embedding or not')
+parser.add_argument('--rl_pretrain', type=bool, default=True, help='rl_pretrain or supervised pretrain')
+
+
 
 opt = parser.parse_args()
 torch.manual_seed(opt.seed)
 torch.set_num_threads(1)
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 tr.init(opt)
 vocab = tr.loadVocab(opt.task)
 ivocab = tr.loadiVocab(opt.task)
@@ -58,20 +64,38 @@ test_dataset = tr.loadData('test',opt.task)
 
 model = compAggWikiqa(opt).cuda()
 
-for i in range(opt.max_epochs):
+# pretrain
+cur_best_MAP = 0.714
+for i in range(opt.pretrain_epochs):
     random.shuffle(train_dataset)
+    rl_pretrain(model, train_dataset,opt,i)
+    results = model.predict_dataset(train_dataset,dev_dataset,test_dataset,rl_predict=opt.rl_pretrain)
+    for j, vals in enumerate(results):
+        if j == 0:
+            print("Dev: MAP: %s, MRR: %s" % (vals[0], vals[1]))
+        else:
+            print("Test: MAP: %s, MRR: %s" % (vals[0], vals[1]))
+            test_MAP = vals[0]
+    if test_MAP > cur_best_MAP:
+        print('saving pretrained parameters')
+        print()
+        torch.save(model.state_dict(),'pos_params.pkl')
+        cur_best_MAP = test_MAP
+
+print('loading pretrained parameters...')
+model.load_state_dict(torch.load('params.pkl'))
+results = model.predict_dataset(train_dataset,dev_dataset,test_dataset,rl_predict=opt.rl_pretrain)
+for j, vals in enumerate(results):
+    if j == 0:
+        print("pretrained Dev: MAP: %s, MRR: %s" % (vals[0], vals[1]))
+    else:
+        print("pretrained Test: MAP: %s, MRR: %s" % (vals[0], vals[1]))
+
+
+# RL training
+for i in range(opt.max_epochs):
+    # random.shuffle(train_dataset)
     train(model, train_dataset,opt,i)
     model.optim_state['learningRate'] = model.optim_state['learningRate'] * opt.lr_decay
-
-    results = model.predict_dataset(train_dataset,dev_dataset,test_dataset)
+    results = model.predict_dataset(train_dataset,dev_dataset,test_dataset,rl_predict=True)
     model.save('../trainedmodel/', opt, results, i)
-
-
-
-    # if i == opt.max_epochs then
-    #     model.params:copy( model.best_params )
-    #     recordDev = model:predict_dataset(dev_dataset)
-    #     if opt.task == 'snli' or opt.task == 'wikiqa' then recordTest   = model:predict_dataset(test_dataset) end
-    #     recordTrain  = model:predict_dataset(train_dataset)
-    #     model.save('../trainedmodel/', opt, {recordDev, recordTest, recordTrain}, i)
-
